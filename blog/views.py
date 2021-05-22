171946dcfd46b9b3cf2a4244d38dfb9ad237b532
Stefan Griffin <griffinstef39@gmail.com>
2021-05-21 20:19:31 -0400

    static files work on

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
+from datetime import date
 from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+
+post = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpeg",
+        "author": "Griffin2021",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "Add a lot of text in here if you feel like it"
+
+    }
+]
+
 
 def starting_page(request):
     return render(request, 'blog/index.html')
@@ -12,4 +26,4 @@ def posts(requests):
 
 
 def post_detail(requests):
-    pass
+    return render(requests, "blog/post-detail.html")
