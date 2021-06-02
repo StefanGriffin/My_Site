e2cb03e34b67d6d3577b5fa54af7eb2c4cac1770
Stefan Griffin <griffinstef39@gmail.com>
2021-06-02 10:32:04 -0400

    code with added explained notes

--- blog/views.py
+++ blog/views.py
@@ -1,74 +1,37 @@
-from datetime import date
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
-
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "1.jpeg",
-        "author": "griffin2021",
-        "date": date(2021, 6, 1),
-        "title": "Mountain Hiking",
-        "excerpt": "The technology of the future !!!",
-        "content": """  Lorem ipsum dolor sit amet, 
-            consectetur adipisicing elit. Libero nemo dolorum veniam eligendi laborum quas beatae, expedita provident asperiores excepturi aut 
-            blanditiis animi facilis obcaecati repudiandae porro. Numquam, id fugit. """
-
-    },
-
-    {
-        "slug": "coding-is-fun",
-        "image": "10.png",
-        "author": "george",
-        "date": date(2019, 2, 22),
-        "title": "Hardcoding bla",
-        "excerpt": "Code your Ideas",
-        "content": """  Lorem ipsum dolor sit amet, 
-            consectetur adipisicing elit. Libero nemo dolorum veniam eligendi laborum quas beatae, expedita provident asperiores excepturi aut 
-            blanditiis animi facilis obcaecati repudiandae porro. Numquam, id fugit. """
-
-    },
-      {
-        "slug": "coding-is-maybe-fun",
-        "image": "9.png",
-        "author": "dan",
-        "date": date(2019, 2, 22),
-        "title": "Hardcoding bla bla",
-        "excerpt": "Code your Ideasddd",
-        "content": """  Lorem ipsum dolor sit amet, 
-            consectetur adipisicing edfdfdsfdsfgflit. Libero nemo dolorum veniam eligendi laborum quas beatae, expedita provident asperiores excepturi aut 
-            blanditiis animi facilis obcaecati repudiandae porro. Numquam, id fugit. """
-
-    }
-]
-
-
-def get_date(post):
-    return post['date']
+# def get_date(post):
+#     return post['date']
 
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]  # this is a sql command 
+    # -date --> desenting order meaning that the last post is gooing to be the first
+    # sorted_posts = sorted(all_posts, key=get_date)
+    # latest_posts = sorted_posts[-3:]
     return render(request, 'blog/index.html', {
         "posts": latest_posts
     })  # connected with path --> starting-page
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, 'blog/all-posts.html', {
         "all_posts": all_posts
     })  # connected with path --> posts-page
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug']==slug)  
+    # identified_post = next(post for post in all_posts if post['slug']==slug)  
     #post['slug'] is the same thing with {{ post.title }} in the post-detail.html template
+    # identified_post = Post.objects.get(slug=slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })  # connected with path --> post-detail-page
